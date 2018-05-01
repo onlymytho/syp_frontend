fb53813aa2468f6d2de7ec53531f7d9947d710bf
Sanghyuk.Jung <onlymytho@gmail.com>
2018-05-01 17:35:33 +0900

    Initial commit

---
+++ static/js/getJSON.js
@@ -0,0 +1,30 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
+
+var postJSON = function(url, params, callback) {
+    var xhr = new XMLHttpRequest();
+    var params; //"lorem=ipsum&name=binny";
+    xhr.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() {//Call a function when the state changes.
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            alert(xhr.responseText);
+        }
+    }
+    xhr.send(params);
+}
