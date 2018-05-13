1987cf89609488c6557e73e59ed4579ed4606275
Sanghyuk.Jung <onlymytho@gmail.com>
2018-05-14 01:02:27 +0900

    Create chalice project

---
+++ chalice/app.py
@@ -0,0 +1,54 @@
+from chalice import Chalice, Response
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+app = Chalice(app_name='syp_frontend')
+
+env = Environment(
+    loader=PackageLoader(__name__, 'chalicelib/templates'),
+    autoescape=select_autoescape(['html', 'xml']))
+
+
+def render_template(template_name, context, status_code=200, content_type='text/html', headers=None):
+    template = env.get_template(template_name)
+    body = template.render(**context)
+
+    if headers is None:
+        headers = {}
+
+    headers.update({
+        'Content-Type': content_type,
+    })
+
+    return Response(body=body, status_code=status_code, headers=headers)
+
+
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+@app.route('/personalize', cors=True)
+def index_test_cors():
+    return render_template('index.html', {
+        'name': 'AskDjango'
+    })
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
---
+++ chalice/chalicelib/static/js/getJSON.js
@@ -0,0 +1,35 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
+
+var postJSON = function(url, params, callback) {
+    var xhr = new XMLHttpRequest();
+    var params = JSON.stringify(params); // as json style string;
+    xhr.open("POST", url, true);
+    //Send the proper header information along with the request
+    // xhr.setRequestHeader("Content-type", "application/json");
+
+    xhr.onreadystatechange = function() {//Call a function when the state changes.
+        if(xhr.readyState == 4) {
+            if(xhr.status == 200) {
+                callback(null, JSON.parse(xhr.responseText));
+            } else {
+                console.log(xhr)
+                callback(xhr.status, null)
+            }
+        }
+    }
+
+    xhr.send(params);
+}
