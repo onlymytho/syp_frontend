c4a51a9626a45b1cc6b321aafb2975e776d11211
Sanghyuk.Jung <onlymytho@gmail.com>
2018-06-08 14:19:57 +0900

    SSR setting for hanpo_content_test

--- chalice/app.py
+++ chalice/app.py
@@ -8,39 +8,56 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml']))
 
 def render_template(template_name, context, status_code=200, content_type='text/html', headers=None):
-    template = env.get_template(template_name)
-    body = template.render(**context)
+    try:
+        template = env.get_template(template_name)
+        body = template.render(**context)
 
-    if headers is None:
-        headers = {}
+        if headers is None:
+            headers = {}
 
-    headers.update({
-        'Content-Type': content_type,
-    })
-    response = Response(body=body, status_code=status_code, headers=headers)
-    print (response.to_dict())
-    return response
+        headers.update({
+            'Content-Type': content_type,
+        })
+        response = Response(body=body, status_code=status_code, headers=headers)
+        print (response.to_dict())
+        return response
+    except:
+        print (error)
 
 
 
-@app.route('/')
+@app.route('/', cors=True)
 def index():
-    return render_template('index.html', {'name': 'home'})
+    print (app.current_request)
+    if (app.current_request.query_params):
+        hanpo_content_test = app.current_request.query_params.get('content')
+        if hanpo_content_test:
+            print ('hanpo_content_test: ' + str(hanpo_content_test))
+            return render_template('hanpo_content/' + hanpo_content_test + '.html', {'name': 'home'})
+        else:
+            return render_template('index.html', {'name': 'home'})
+    else:
+        return render_template('index.html', {'name': 'home'})
+
 
 @app.route('/personalize', cors=True)
 def personalize():
-    return render_template('index.html', {'name': 'personalize'})
+    return render_template('personalize.html', {'name': 'personalize'})
+
 
 @app.route('/personalize/result', cors=True)
 def result():
-    print ("/personalize/result")
-    print (app.current_request)
-    recipe_id = app.current_request.query_params.get('recipe_id')
-    print (recipe_id)
-    if recipe_id:
-        return render_template('result.html', {'recipe_id': recipe_id})
-    else:
-        return Response(status_code=422)
+    try:
+        print ("/personalize/result")
+        print (app.current_request)
+        recipe_id = app.current_request.query_params.get('recipe_id')
+        print (recipe_id)
+        if recipe_id:
+            return render_template('result.html', {'recipe_id': recipe_id})
+        else:
+            return Response(status_code=422)
+    except:
+        print(error)
 
 
 # The view function above will return {"hello": "world"}
